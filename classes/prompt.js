bdb9ec213780da65308a9102ae1e19a6b43fada7
Juan Pablo Garcia <45505790+JPGarCar@users.noreply.github.com>
2020-12-29 20:56:20 -0800

    added number prompt
    Added a second static method, it prompts for and returns a number.

--- classes/prompt.js
+++ classes/prompt.js
@@ -7,16 +7,16 @@ class Prompt {
 
     /**
      * Prompt the user for some text.
-     * @param {string} prompt - the text prompt to send to user
-     * @param {string} responseType - the type of response, one of string, number, boolean
+     * @param {String} prompt - the text prompt to send to user
+     * @param {String} responseType - the type of response, one of string, number, boolean
      * @param {Channel} channel - the channel to send the prompt to
-     * @param {string} userID - the ID of the user to prompt
+     * @param {String} userID - the ID of the user to prompt
      * @returns {Promise<Message>} - the message response to the prompt
      * @async
      */
     static async messagePrompt(prompt, responseType, channel, userID) {
         // send prompt
-        let promptMsg = await channel.send('<@' + userID + '> ' + prompt);
+        let promptMsg = await channel.send('<@' + userID + '> ' + prompt + responseType === 'number' ? ' Respond a number only!' : '');
 
         let msgs = await channel.awaitMessages(message => message.author.id === userID, {max: 1});
 
@@ -26,6 +26,23 @@ class Prompt {
         msg.delete();
 
         return msg;
+    }
+
+
+    /**
+     * Prompt a user for a number, will ask again if not given a number.
+     * @param {String} prompt - the text prompt to send to user
+     * @param {Channel} channel - the channel to send the prompt to
+     * @param {String} userID - the ID of the user to prompt
+     * @async
+     * @returns {Promise<Number>} - the number gotten from the prompt
+     */
+    static async numberPrompt(prompt, channel, userID) {
+        let promtMsg = await this.messagePrompt(prompt, 'number', channel, userID);
+        let number = parseInt(promtMsg.content);
+        if (isNan(number)) return this.numberPrompt(prompt, channel, userId);
+        else return number;
+
     }
 }
 
