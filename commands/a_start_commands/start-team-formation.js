7bf13d3d7f76a193d52b962e8ad4357a024c4c57
Juan Pablo Garcia <45505790+JPGarCar@users.noreply.github.com>
2021-01-01 16:32:57 -0800

    Major improvements! and new roles!
    Use of inline checks, use of class properties, better helper function documentation. Added specific looking for team or lookign for members roles. They can be created or given by admin. Will be used to tag the other party when new posts are made.

--- commands/a_start_commands/start-team-formation.js
+++ commands/a_start_commands/start-team-formation.js
@@ -2,6 +2,7 @@
 const PermissionCommand = require('../../classes/permission-command');
 const discordServices = require('../../discord-services');
 const Discord = require('discord.js');
+const Prompt = require('../../classes/prompt.js');
 
 // Command export
 module.exports = class StartTeamFormation extends PermissionCommand {
@@ -21,8 +22,60 @@ module.exports = class StartTeamFormation extends PermissionCommand {
         });
     }
 
-  
+    /**
+     * 
+     * @param {Discord.Message} message - the message in which the command was run
+     */
     async runCommand(message) {
+
+        /**
+         * The looking for team emoji used for this command.
+         * @type {String} - an emoji string
+         */
+        this.lftEmoji = '🏍️';
+
+        /**
+         * The looking for members emoji used for this command.
+         * @type {String} - an emoji string
+         */
+        this.lfmEmoji = '🚎';
+
+        /**
+         * The role for those who are looking for a team!
+         * @type {Discord.Role}
+         */
+        this.lookingForTeamRole;
+
+        /**
+         * The role for those who are looking for members!
+         * @type {Discord.Role}
+         */
+        this.lookingForMembersRole;
+
+        // create or find the two roles to be used
+        let areCreated = await Prompt.yesNoPrompt('Are the team formation roles already created?', message.channel, message.author.id);
+
+        if (areCreated) {
+            let lftRoleMsg = await Prompt.messagePrompt('Please tag the looking for team role!', 'string', message.channel, message.author.id);
+            this.lookingForTeamRole = lftRoleMsg.mentions.roles.first();
+            let lfmRoleMsg = await Prompt.messagePrompt('Please tag the looking for members role!', 'string', message.channel, message.author.id);
+            this.lookingForMembersRole = lfmRoleMsg.mentions.roles.first();
+        } else {
+            this.lookingForTeamRole = await message.guild.roles.create({
+                data: {
+                    name: 'looking for team',
+                    color: discordServices.tfHackerEmbedColor,
+                }
+            });
+
+            this.lookingForMembersRole = await message.guild.roles.create({
+                data: {
+                    name: 'looking for members',
+                    color: discordServices.tfTeamEmbedColor,
+                }
+            });
+        }
+
         // grab current channel
         var channel = message.channel;
                 
@@ -33,133 +86,127 @@ module.exports = class StartTeamFormation extends PermissionCommand {
             .setDescription('Welcome to the team formation section! If you are looking for a team or need a few more members to complete your ultimate group, you are in the right place!')
             .addField('How does this work?', '* Once you react to this message, I will send you a template you need to fill out and send back to me via DM. \n* Then I will post your information in recruiting or looking-for-team channel. \n* Then, other hackers, teams, or yourself can browse these channels and reach out via DM!')
             .addField('Disclaimer!!', 'By participating in this activity, you consent to let the nwPlus bot initiate a conversation between you and other teams or hackers.')
-            .addField('Teams looking for new members', 'React with 🚎 and the bot will send you instructions.')
-            .addField('Hacker looking for a team', 'React with 🏍️ and the bot will send you instructions.')
+            .addField('Teams looking for new members', 'React with ' + this.lfmEmoji + ' and the bot will send you instructions.')
+            .addField('Hacker looking for a team', 'React with ' + this.lftEmoji + ' and the bot will send you instructions.')
         
         var cardMessage = await channel.send(msgEmbed);
-
-        await cardMessage.react('🚎');
-        await cardMessage.react('🏍️');
+        cardMessage.react(this.lfmEmoji);
+        cardMessage.react(this.lftEmoji);
 
         // collect form reaction collector and its filter
-        const emojiFilter = (reaction, user) => !user.bot && (reaction.emoji.name === '🏍️' || reaction.emoji.name === '🚎');
-        var mainCollector = await cardMessage.createReactionCollector(emojiFilter);
+        const emojiFilter = (reaction, user) => !user.bot && (reaction.emoji.name === this.lftEmoji || reaction.emoji.name === this.lfmEmoji);
+        var mainCollector = cardMessage.createReactionCollector(emojiFilter);
 
         mainCollector.on('collect', async (reaction, user) => {
-            await this.reachOutToHacker(reaction, user, message);
+
+            let isTeam = reaction.emoji.name === this.lfmEmoji;
+
+            await this.reachOutToHacker(user, message.guild, isTeam);
         });
         
     }
 
 
-    // send message with instructions to hacker, will also collect the form to send it to the correct channel
-    async reachOutToHacker(reaction, user, message) {
-        // boolean if team or hacker, depends on emoji used
-        var isTeam = reaction.emoji.name === '🚎';
+    /**
+     * send message with instructions to hacker, will also collect the form to send it to the correct channel
+     * @param {Discord.User} user - the user
+     * @param {Discord.Guild} guild - the message where the command was run
+     * @param {Boolean} isTeam - weather or no the user is a team
+     */
+    async reachOutToHacker(user, guild, isTeam) {
 
         const dmMessage = new Discord.MessageEmbed();
 
-        // branch between team and hacker
-        if (isTeam) {
-            dmMessage.setTitle('Team Formation - Team Format');
-            dmMessage.setDescription('We are very excited for you to find your perfect team members! \n* Please **copy and paste** the following format in your next message. ' +
+        dmMessage.setTitle('Team Formation -' + (isTeam ? ' Team Format' : ' Hacker Format'))
+            .setDescription('We are very excited for you to find your perfect ' + (isTeam ? 'team members.' : 'team.') + '\n* Please **copy and paste** the following format in your next message. ' +
                 '\n* Try to respond to all the sections! \n* Once you are ready to submit, react to this message with 🇩 and then send me your information!\n' +
-                '* Once you find a hacker, please come back and click the ⛔ emoji.');
-            dmMessage.addField('Format:', 'Team Member(s): \nTeam Background: \nObjective: \nFun Fact About Team: \nLooking For: ');
-        } else {
-            dmMessage.setTitle('Team Formation - Hacker Format');
-            dmMessage.setDescription('We are very excited for you to find your perfect team! \n* Please copy and paste the following format in your next message. ' +
-                '\n* Try to respond to all the sections! \n* Once you are ready to submit, react to this message with 🇩 and then send me your information!\n' +
-                '* Once you find a team, please come back and click the ⛔ emoji.');
-            dmMessage.addField('Format:', 'Name: \nSchool: \nPlace of Origin: \nSkills: \nFun Fact: ');
-        }
+                '* Once you find a hacker, please come back and click the ⛔ emoji.')
+            .addField('Format:', isTeam ? 'Team Member(s): \nTeam Background: \nObjective: \nFun Fact About Team: \nLooking For: ' : 
+                                                'Name: \nSchool: \nPlace of Origin: \nSkills: \nFun Fact: ')
+            .addField('READ THIS!', 'As soon as you submit your form, you will be notified of every new ' + (isTeam ? 'available hacker.' : 'available team.') + 
+                                    'Once you close your form, you will stop receiving notifications!');
 
         // send message to hacker via DM
-        var dmMsg = await user.send(dmMessage);
+        let dmMsg = await user.send(dmMessage);
         dmMsg.react('🇩');  // emoji for user to send form to bot
 
         // guard
-        var isResponding = false;
+        let isResponding = false;
         
         // user sends form to bot collector and filter
         const dmCollector = dmMsg.createReactionCollector((reaction, user) => !user.bot && !isResponding && (reaction.emoji.name === '🇩'));
 
         dmCollector.on('collect', async (reaction, user) => {
             isResponding = !isResponding;
-            await this.gatherForm(user, isTeam, message, dmMsg, dmCollector, isResponding);
+            await this.gatherForm(user, guild, dmMsg, dmCollector, isResponding, isTeam);
             // can not remove reaciton on DMs -> sad!
         });
     }
 
 
-    // will get the form from the user in the DM and publish it in the correct channel
-    async gatherForm(user, isTeam, message, dmMsg, collector, isResponging) {
-        // promt user
-        var confDm = await user.send('Please send me your completed form, if you do not follow the form your post will be deleted! You have 10 seconds to send your information.');
+    /**
+     * Grabs the form from the user and post it in the correct channel. Will also asign roles.
+     * @param {Discord.User} user - the user
+     * @param {Discord.Guild} guild - the original command message
+     * @param {Discord.Message} dmMsg - the first DM message with instructions
+     * @param {Discord.ReactionCollector} collector - the reaction collector
+     * @param {Boolean} isResponging - if the user is already responding
+     * @param {Boolean} isTeam - weather or not the user is a team
+     */
+    async gatherForm(user, guild, dmMsg, collector, isResponging, isTeam) {
 
-        // await form from user for 10 seconds max
-        confDm.channel.awaitMessages(m => !m.author.bot, { max: 1, time: 10000, errors: ['time'] }).then(async (msgs) => {
-            // user msg and its content (form)
-            var msg = msgs.first();
-            var content = msg.content;
+        let formMsg = await Prompt.messagePrompt('Please send me your completed form, if you do not follow the form your post will be deleted! You have 10 seconds to send your information.', 
+                                                    'string', user.dmChannel, user.id, 10);
+        
 
-            var sentMessage;
+        // check if the prompt timed out, if so, exit
+        if (formMsg === false) {
+            isResponging = !isResponging;
+            return;
+        }
 
-            const publicEmbed = new Discord.MessageEmbed()
+        const publicEmbed = new Discord.MessageEmbed()
                 .setTitle('Information about them can be found below:')
-                .setDescription(content + '\nDM me to talk -> <@' + msg.author.id + '>');
+                .setDescription(formMsg.content + '\nDM me to talk -> <@' + user.id + '>')
+                .setColor(isTeam ? discordServices.tfTeamEmbedColor : discordServices.tfHackerEmbedColor);
 
-            if (isTeam) {
-                // set color
-                publicEmbed.setColor(discordServices.tfTeamEmbedColor);
-                // get recruiting channel and send message
-                var channel = message.guild.channels.cache.get(discordServices.recruitingChannel);
-                sentMessage = await channel.send('<@' + user.id + '> and their team are looking for more team members!', {embed: publicEmbed});
-            } else {
-                //
-                publicEmbed.setColor(discordServices.tfHackerEmbedColor);
-                // get looking for team channel and send message
-                var channel = message.guild.channels.cache.get(discordServices.lookingforteamChannel);
-                sentMessage = await channel.send('<@' + user.id + '>  is looking for a team to join!', {embed: publicEmbed});
-            }
+        let channel = guild.channels.cache.get(isTeam ? discordServices.recruitingChannel : discordServices.lookingforteamChannel);
 
-            // we would want to remove their message, but that is not possible!
+        let sentMessage = await channel.send('<@&' + (isTeam ? this.lookingForTeamRole : this.lookingForMembersRole) + '>, <@' + 
+                                                user.id + (isTeam ? '> and their team are looking for more team members!' : '>  is looking for a team to join!'), {embed: publicEmbed});
 
 
-            // add remove form emoji and collector
-            await dmMsg.react('⛔');
+        // confirm the post has been received
+        discordServices.sendMessageToMember(user ,isTeam ? 'Thanks for sending me your information, you should see it pop up in the respective channel under the team formation category.' +
+            'Once you find your members please react to my original message with ⛔ so I can remove your post. Happy hacking!!!' : 
+            'Thanks for sending me your information, you should see it pop up in the respective channel under the team formation category.' +
+            'Once you find your ideal team please react to my original message with ⛔ so I can remove your post. Happy hacking!!!', true);
 
-            const removeFilter = (reaction, user) => reaction.emoji.name === '⛔' && !user.bot;
-            const removeCollector = await dmMsg.createReactionCollector(removeFilter, { max: 1 });
+        // stop the first collector to add a new one for removal
+        collector.stop();
+        isResponging = !isResponging;
 
-            removeCollector.on('collect', async (reac, user) => {
-                // remove message sent to channel
-                discordServices.deleteMessage(sentMessage);
+        // add role to the user
+        discordServices.addRoleToMember(guild.member(user), isTeam ? this.lookingForMembersRole : this.lookingForTeamRole);
 
-                // confirm deletion
-                user.send('This is great! You are now ready to hack! Have fun with your new team! Your message has been deleted.').then(msg => msg.delete({ timeout: 5000 }));
 
-                // remove this message
-                dmMsg.delete();
-            });
+        // add remove form emoji and collector
+        dmMsg.react('⛔');
 
-            // confirm the post has been received
-            if (isTeam) {
-                user.send('Thanks for sending me your information, you should see it pop up in the respective channel under the team formation category.' +
-                    'Once you find your members please react to my original message with ⛔ so I can remove your post. Happy hacking!!!').then(msg => msg.delete({ timeout: 8000 }));
-            } else {
-                user.send('Thanks for sending me your information, you should see it pop up in the respective channel under the team formation category.' +
-                    'Once you find your ideal team please react to my original message with ⛔ so I can remove your post. Happy hacking!!!').then(msg => msg.delete({ timeout: 8000 }));
-            }
+        const removeFilter = (reaction, user) => reaction.emoji.name === '⛔' && !user.bot;
+        const removeCollector = dmMsg.createReactionCollector(removeFilter, { max: 1 });
 
-            // remove the promt message from the bot in the DM channel
-            confDm.delete();
-            collector.stop();
-            isResponging = !isResponging;
-        }).catch((reason) => {
-            confDm.delete();
-            isResponging = !isResponging;
-            user.send('Time is up! Write up your response and react to the emoji again!').then(msg => msg.delete({timeout: 3000}));
+        removeCollector.on('collect', async (reac, user) => {
+            // remove message sent to channel
+            discordServices.deleteMessage(sentMessage);
+
+            // confirm deletion
+            discordServices.sendMessageToMember(user, 'This is great! You are now ready to hack! Have fun with your new team! Your message has been deleted.', true);
+
+            discordServices.removeRolToMember(guild.member(user), isTeam ? this.lookingForMembersRole : this.lookingForTeamRole);
+
+            // remove this message
+            dmMsg.delete();
         });
     }
 }
