07e9f5addf2b55f2292c8667264729a7e2767a54
Juan Pablo Garcia <45505790+JPGarCar@users.noreply.github.com>
2021-01-02 19:38:16 -0800

    Update prompt.js
    Cancel bug fixes and better string UI.

--- classes/prompt.js
+++ classes/prompt.js
@@ -19,7 +19,7 @@ class Prompt {
     static async messagePrompt(prompt, responseType, channel, userID, time = 0) {
 
         let finalPrompt = '<@' + userID + '> ' + prompt + (responseType == 'number' ? ' Respond with a number only!' : responseType == 'boolean' ? ' (yes/no)' : responseType == 'mention' ? ' To make a mention use the @ or # for a user or channel respectively!' : '' + 
-                        (time === 0 ? '' : 'Respond within ' + time + ' secodns.') + 'Respond with cancel to cancel.');
+                        (time === 0 ? '' : '\n* Respond within ' + time + ' secodns.') + '\n* Respond with cancel to cancel.');
 
         // send prompt
         let promptMsg = await channel.send(finalPrompt);
@@ -32,7 +32,7 @@ class Prompt {
             discordServices.deleteMessage(msg);
 
             // check if they responded with cancel
-            if (msg.content.toLocaleLowerCase() === 'cancel') {
+            if (msg.content.toLowerCase() === 'cancel') {
                 return null;
             }
 
