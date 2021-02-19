522481e006da37a76f47424b8d58098a69409cef
maggiew7 <maggiewang759@gmail.com>
2021-02-18 19:26:50 -0800

    tweaked small features of manual-verify

--- commands/verification/manual-verify.js
+++ commands/verification/manual-verify.js
@@ -17,18 +17,16 @@ module.exports = class Verification extends PermissionCommand {
         },
         {
             role: PermissionCommand.FLAGS.STAFF_ROLE,
-            roleMessage: 'Hey there, the !manual-verify command is only for staff!',
-            channel: PermissionCommand.FLAGS.ADMIN_CONSOLE,
-            channelMessage: 'Hey there, the !manual-verify command is only available in the admin console channel.',
+            roleMessage: 'Hey there, the !manual-verify command is only for staff!'
         });
     }
 
     async runCommand(message) {
         let members = await Prompt.memberPrompt('Which member(s) would you like to verify? (They have to receive the same role!)', message.channel, message.author);
-        let newRole = await Prompt.rolePrompt('Which role would you like to verify them to?', message.channel, message.author).first().id;
+        let newRole = (await Prompt.rolePrompt('Which role would you like to verify them to?', message.channel, message.author)).first().id;
         members.each((key, member) => {
             if (!discordServices.checkForRole(member, discordServices.roleIDs.guestRole)) {
-                message.channel.send(member + 'is not a guest!');
+                message.channel.send(member + 'is not a guest!').then(msg => msg.delete({timeout: 3000}));
             } else {
                 discordServices.replaceRoleToMember(member, newRole);
             }
