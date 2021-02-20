775bd209deaf56a13f08825758595f6b0eff183e
Juan Pablo Garcia <45505790+JPGarCar@users.noreply.github.com>
2021-02-19 16:23:37 -0800

    Removed email from !attend command as its not needed.

--- commands/verification/attend.js
+++ commands/verification/attend.js
@@ -15,12 +15,6 @@ module.exports = class Attend extends PermissionCommand {
             description: 'Will mark a hacker as attending and upgrade role to Attendee. Can only be called once!',
             args: [
                 {
-                    key: 'email',
-                    prompt: 'Please provide your email address',
-                    type: 'string',
-                    default: '',
-                },
-                {
                     key: 'guildId',
                     prompt: 'Please provide the server ID, ask admins for it!',
                     type: 'integer',
@@ -35,27 +29,15 @@ module.exports = class Attend extends PermissionCommand {
     /**
      * 
      * @param {Discord.Message} message 
-     * @param {String} param1 
+     * @param {String} guildId 
      */
-    async runCommand(message, { email, guildId }) {
+    async runCommand(message, { guildId }) {
         // check if the user needs to attend, else warn and return
         if (discordServices.checkForRole(message.author, discordServices.roleIDs.attendeeRole)) {
             discordServices.sendEmbedToMember(message.author, {
                 title: 'Attend Error',
                 description: 'You do not need to attend! Happy hacking!!!'
             }, true);
-            return;
-        }
-
-        // make email lowercase
-        email = email.toLowerCase();
-
-        // regex to validate email
-        const re = /^(([^<>()[\]\.,;:\s@\"]+(\.[^<>()[\]\.,;:\s@\"]+)*)|(\".+\"))@(([^<>()[\]\.,;:\s@\"]+\.)+[^<>()[\]\.,;:\s@\"]{2,})$/i;
-
-        // let user know he has used the command incorrectly and exit
-        if (email === '' || !re.test(email)) {
-            discordServices.sendMessageToMember(message.author, 'You have used the verify command incorrectly! \nPlease write a valid email after the command like this: !verify email@gmail.com');
             return;
         }
 
