e971509cc18430da87ad6eb3c98226f9d96b1362
Juan Pablo Garcia <45505790+JPGarCar@users.noreply.github.com>
2021-03-08 15:34:16 -0800

    Installed eslint with settings

---
+++ .eslintrc.js
@@ -0,0 +1,29 @@
+module.exports = {
+    'env': {
+        'browser': true,
+        'commonjs': true,
+        'es2021': true
+    },
+    'extends': 'eslint:recommended',
+    'parserOptions': {
+        'ecmaVersion': 12
+    },
+    'rules': {
+        'indent': [
+            'error',
+            4
+        ],
+        'linebreak-style': [
+            'error',
+            'windows'
+        ],
+        'quotes': [
+            'error',
+            'single'
+        ],
+        'semi': [
+            'error',
+            'always'
+        ]
+    }
+};
