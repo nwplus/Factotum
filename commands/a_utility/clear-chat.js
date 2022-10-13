25c4fed44e8f83cc78a046d1a32742b650f561ef
maggiew7 <maggiewang759@gmail.com>
2022-10-12 20:42:23 -0700

    port clear-chat

--- commands/a_utility/clear-chat.js
+++ commands/a_utility/clear-chat.js
@@ -1,57 +1,40 @@
-// Discord.js commando requirements
-const PermissionCommand = require('../../classes/permission-command');
+const { Command } = require('@sapphire/framework');
+const { Interaction } = require('discord.js');
 const { discordLog } = require('../../discord-services');
-const { MessageEmbed, Message } = require('discord.js');
-const BotGuildModel = require('../../classes/Bot/bot-guild');
 
-/**
- * The clear chat command will clear a channel from at most 100 messages that are at least 2 weeks young.
- * Option to keep pinned messages available.
- * @category Commands
- * @subcategory Admin-Utility
- * @extends PermissionCommand
- */
-class ClearChat extends PermissionCommand {
-    constructor(client) {
-        super(client, {
-            name: 'clear-chat',
-            group: 'a_utility',
-            memberName: 'clear chat utility',
-            description: 'Will clear up to 100 newest messages from the channel. Messages older than two weeks will not be deleted. Then will send message with available commands in the channel, if any.',
-            guildOnly: true,
-            args: [
-                {
-                    key: 'keepPinned',
-                    prompt: 'if pinned messages should be kept',
-                    type: 'boolean',
-                    default: false,
-                },
-            ],
-        },
-        {
-            role: PermissionCommand.FLAGS.STAFF_ROLE,
-            roleMessage: 'Hey there, the command !clear-chat is only available to staff!',
+
+class ClearChat extends Command {
+    constructor(context, options) {
+        super(context, {
+          ...options,
+          description: 'Clear most recent 100 messages younger than 2 weeks.'
         });
     }
 
+    registerApplicationCommands(registry) {
+        registry.registerChatInputCommand((builder) =>
+          builder
+            .setName(this.name)
+            .setDescription(this.description)
+        )
+    }
+
     /**
-     * @param {BotGuildModel} botGuild - the botGuild instance given from PermissionCommand
-     * @param {Message} message - the message in which the command was run
+     * @param {Interaction} interaction
      * @param {Object} args - the command arguments
      * @param {Boolean} args.keepPinned - if true any pinned messages will not be removed
      */
-    async runCommand (botGuild, message, {keepPinned}) {
+    async chatInputRun (interaction, {keepPinned}) {
 
         if (keepPinned) {
             // other option is to get all channel messages, filter of the pined channels and pass those to bulkDelete, might be to costly?
-            var messagesToDelete = message.channel.messages.cache.filter(msg => !msg.pinned);
-            await message.channel.bulkDelete(messagesToDelete, true).catch(console.error);
+            var messagesToDelete = interaction.channel.messages.cache.filter(msg => !msg.pinned);
+            await interaction.channel.bulkDelete(messagesToDelete, true).catch(console.error);
         } else {
             // delete messages and log to console
-            await message.channel.bulkDelete(100, true).catch(console.error);
+            await interaction.channel.bulkDelete(100, true).catch(console.error);
         }
-
-        discordLog(message.guild, 'CHANNEL CLEAR ' + message.channel.name + '. By user: ' + message.author.username);
+        discordLog(interaction.guild, 'CHANNEL CLEAR ' + interaction.channel.name);
     }
 }
 module.exports = ClearChat;
